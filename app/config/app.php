dc4c402b3e158f37f61770e3f7bc3bf4423f83c5
nrs365 <nrs365@gmail.com>
2014-07-18 12:06:44 -0500

    commiting files

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Thujohn\Twitter\TwitterServiceProvider'
 
 	),
 
@@ -185,6 +186,7 @@ return array(
 		'SoftDeletingTrait' => 'Illuminate\Database\Eloquent\SoftDeletingTrait',
 		'SSH'             => 'Illuminate\Support\Facades\SSH',
 		'Str'             => 'Illuminate\Support\Str',
+		'Twitter' => 'Thujohn\Twitter\TwitterFacade',
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
