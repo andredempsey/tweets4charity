bd0b0837706a171fef2322a176a9b0a35931f504
nrs365 <nrs365@gmail.com>
2014-07-22 09:43:33 -0500

    migrations table for adding twitter oauth tokens

--- app/database/migrations/2014_07_14_203458_create_users_table.php
+++ app/database/migrations/2014_07_14_203458_create_users_table.php
@@ -15,7 +15,13 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('first_name', 20);
+			$table->string('last_name', 20);
 			$table->string('twitter_handle', 20)->unique();
+			$table->string('oauth_token', 100);
+			$table->string('oauth_token_secret', 100)->unique();
+			$table->string('user_id', 50)->unique();
+			// $table->screen_name('screen_name', 20);
 			$table->string('profile_picture_link', 200);
 			$table->string('email', 50);
 			$table->string('password', 200);
