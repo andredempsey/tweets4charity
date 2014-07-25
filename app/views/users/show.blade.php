ffc01019bd59fb46b2447502b9e1bc036fbf76bb
Andre Dempsey <andre.dempsey@live.com>
2014-07-25 09:56:17 -0500

    small formatting changes to donor public profile

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -8,21 +8,26 @@
  <div class="container">
 
         <div class="row">
-            @if(Auth::check() && (Auth::user()->twitter_handle == $user->twitter_handle || Auth::user()->role_id == 'admin'))
-            <div><a href="{{ action('UsersController@edit', $user->twitter_handle) }}">Edit</a></div>
-            @endif
-            <div class="col-lg-4 col-sm-6">
+            
+            <div class="col-lg-12 col-sm-12 pull-left">
                 <a href="https://twitter.com/{{{ $user->twitter_handle }}}"><img class="img-circle" src="{{{ str_replace("normal.jpeg", "400x400.jpeg", $profile_image) }}}" height="200" width="200"></a><!-- <a href="http://www.twitter.com/210AxS"><i class="icon-twitter"></i></a> -->
-            </div>    
-            <div class="col-md-12">
-                <h1 class="page-header">{{{ $user->twitter_handle }}} <a href="http://www.twitter.com/{{{ $user->twitter_handle }}}"><i class="icon-twitter"></i></a>
-
-                    <h3>{{{$statuses_count}}}</h3>
-                    <small>Charities {{{ $name }}} donates to: </small>
+            </div>
+            <div class="row">
+                <div class="col-md-6 pull-left">
+                    <h1 class="page-header">@{{{ $user->twitter_handle }}} <a href="http://www.twitter.com/{{{ $user->twitter_handle }}}"><i class="icon-twitter"></i></a>
+                </div>
+                <div class="col-md-6" pull-right>
+                    @if(Auth::check() && (Auth::user()->twitter_handle == $user->twitter_handle || Auth::user()->role_id == 'admin'))
+                    <h4><a href="{{ action('UsersController@edit', $user->twitter_handle) }}">Edit</a></h4>
+                    @endif    
+                </div>
+            </div>
+            <div class="row">
+                    <h3>Total Tweets:  {{{$statuses_count}}}</h3>
+                    <small>{{{ $name }}} donates to these charities: </small>
 
                 </h1>
             </div>
-            
             <div class="col-md-12">
                 <h2 class="page-header">Charities</h2>
             </div>
